5ceeef6ba51a3069187944f3bd0d0b905f413e94
Bishocko1 <cyprien.dhenain@gmail.com>
2019-05-24 18:32:17 +0200

    v2 <3

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+    res.send('Please run the index.html as a separate server (using webstorm: right click on index.html > open in browser)')
 });
 
 io.on('connection', function(socket){
